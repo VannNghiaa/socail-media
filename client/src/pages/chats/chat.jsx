fa15bf30e6534114f02b395cc9a80fa4a4bbfe34
VannNghia <truongvannghia082004@gmail.com>
2023-11-01 23:23:53 +0700

    add chat

--- client/src/pages/chats/chat.jsx
+++ client/src/pages/chats/chat.jsx
@@ -1,41 +1,67 @@
 import ChatUser from "../../components/chat-people/chat-people";
+import Navbar from "../../components/navbar/navbar";
 import "./chat.css";
 
 function Chat() {
     return (
-        <div className="chat">
-            <div className="chat-people">
-                <div className="chat-header">
-                    <div className="myid">
-                    <span className="myid-style">august.vannghia</span>
-                    </div>
-                    <div className="create-chatnew">
-                        Icon
-                    </div>
-                </div>
-                <div className="chat-midcontent">
-                    <div className="chat-title">
+        <div className="chat-main">
+            <Navbar />
+            <div className="chat">
+                <div className="chat-people">
+                    <div className="chat-header">
+                        <div className="myid">
+                            <span className="myid-style">august.vannghia</span>
+                        </div>
+                        <div className="switch-account">
 
+                        </div>
+                        <div className="chatnew">
+
+                        </div>
                     </div>
-                    <div className="chat-watting">
-
+                    <div className="chat-midcontent">
+                        <div className="chat-title">
+                            <span className="title">Messages</span>
+                        </div>
+                        <div className="chat-request">
+                            {/* <span className="request">Requests</span> */}
+                            <button className="request">Requests</button>
+                        </div>
                     </div>
-                </div>
-                <ChatUser/>
-                <ChatUser/>
-                <ChatUser/>
-                <ChatUser/>
-                <ChatUser/>
-            </div>
-            <div className="chat-content">
-                <div className="content-right">
-
-                </div>
-                <div className="content-left">
-
-                </div>
-                <div className="chat-input">
                     
+                    <div  className="chat-user1">
+                        <ChatUser />
+                        <ChatUser />
+                        <ChatUser />
+                        <ChatUser />
+                        <ChatUser />
+                        <ChatUser />
+                        <ChatUser />
+                        <ChatUser />
+                        <ChatUser />
+                        <ChatUser />
+                        <ChatUser />
+                        <ChatUser />
+                        <ChatUser />
+                        </div>
+                </div>
+                <div className="chat-content">
+                    <div className="chat-content-header">
+                        <div className="avt-person">
+                            <ChatUser />
+                        </div>
+                    </div>
+                    <div className="chat-content-mid">
+                        <div className="content-right">
+
+                        </div>
+                        <div className="content-left">
+
+                        </div>
+                    </div>
+                    <div className="chat-content-footer">
+                        <input className="chat-input" type="text" placeholder="Message..." />
+                    </div>
                 </div>
             </div>
         </div>
